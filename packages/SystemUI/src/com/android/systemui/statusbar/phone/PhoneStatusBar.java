e23ea49846c5eb42c5110e6e2f323875a7ed44c5
Danesh Mondegarian <daneshm90@gmail.com>
2014-11-14 09:44:50 -0800

    Battery Style and percentage (Squashed) (1/2)
    All the below commits merged for marshmallow:
    
    SystemUI : Make battery percentage configurable (1/2)
    by zephiK
    
    Squashed:
    Author: DvTonder
    Fix battery % always showing at < 100%
    https://github.com/CyanogenMod/android_frameworks_base/commit/a1bab878eb11ccb70df09c7876c7baa06fd0e49d
    
    Author:kecinzer
    Battery text mode (1/2)
    https://github.com/CyanogenMod/android_frameworks_base/commit/040721140b6dfe6a4ae5c0db7251073163f19b5d
    
    Author: EnderBlue
    Forward port battery icon options [1/2]
    https://github.com/CyanogenMod/android_frameworks_base/commit/0e31558822b1920ad39e4ba87f813b0bf823a61e
    
    Author: mikeNG
    Cleanup battery style code
    https://github.com/CyanogenMod/android_frameworks_base/commit/9203171b83f530b2948557d0207c70799308c879
    
    Author: maniac103
    Clean up battery handling.
    cffcdc5e20fc729174c650393409ca56e6408bee
    
    Author: LorDClockaN
    FWB: Dotted circle battery (1/2)
    https://github.com/AICP/frameworks_base/commit/84314f10bb2eb14ed560d95a7cf562029e18162c
    
    Some 6.0 changes and partial revert of commit:
    Tuner: add battery pct preference.
    https://github.com/android/platform_frameworks_base/commit/f40d08f8a3094ac5d5478efc5de0b96f57d2c5df
    
    TODO: setDarkIntensity in BatteryMeterView needs attention
    
    Change-Id: Ibfc0cf9c5a4efc13ecd3c3e99723bb2b717c50a7
    
    SystemUI: improve circle battery
    
    While plugged in, the circle animation would cause the statusbar to
    keep redrawing itself repeatedly, using up unnecessary cpu cycles,
    even when idle.
    
    - Removed circle animation in status bar
    - Left animations on keyguard/notificaiton header
    - Set layer type to hardware when animating for optimized quick drawing
    - Cleaned up some logic and unnecessary locking
    - Removed extra work done in each draw
    - override onDraw instead of draw to let the View system decide when we
      should draw
    
    Change-Id: If0624ab6e3723f4522ebbd9ce583b142c1cb4606
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: Fix battery padding
    
    Change-Id: I70ee56d3281ea7e8cf0380824784d2c2cee4b021
    
    SAOSP-M: Dont change "signal_cluster_battery_padding".
    Looks perfect at 7dp
    
    Conflicts:
    	packages/SystemUI/res/xml/tuner_prefs.xml
    	packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/krexus_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -110,6 +110,7 @@ import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.BatteryMeterView;
+import com.android.systemui.BatteryLevelTextView;
 import com.android.systemui.DemoMode;
 import com.android.systemui.EventLogConstants;
 import com.android.systemui.EventLogTags;
@@ -922,7 +923,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // Other icons
         mLocationController = new LocationControllerImpl(mContext,
                 mHandlerThread.getLooper()); // will post a notification
-        mBatteryController = new BatteryController(mContext);
+        mBatteryController = new BatteryController(mContext, mHandler);
         mBatteryController.addStateChangedCallback(new BatteryStateChangeCallback() {
             @Override
             public void onPowerSaveChanged() {
@@ -933,6 +934,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
             @Override
             public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
+                // noop
+            }
+            @Override
+            public void onBatteryStyleChanged(int style, int percentMode) {
                 // noop
             }
         });
@@ -1032,8 +1037,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mUserInfoController.reloadUserInfo();
 
         mHeader.setBatteryController(mBatteryController);
-        ((BatteryMeterView) mStatusBarView.findViewById(R.id.battery)).setBatteryController(
-                mBatteryController);
+        BatteryMeterView batteryMeterView =
+                ((BatteryMeterView) mStatusBarView.findViewById(R.id.battery));
+        batteryMeterView.setBatteryController(mBatteryController);
+        batteryMeterView.setAnimationsEnabled(false);
+        ((BatteryLevelTextView) mStatusBarView.findViewById(R.id.battery_level_text))
+                .setBatteryController(mBatteryController);
         mKeyguardStatusBar.setBatteryController(mBatteryController);
         mHeader.setNextAlarmController(mNextAlarmController);
 
@@ -3175,6 +3184,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         if (mSecurityController != null) {
             mSecurityController.onUserSwitched(mCurrentUserId);
+        }
+        if (mBatteryController != null) {
+            mBatteryController.setUserId(mCurrentUserId);
         }
     }
 
