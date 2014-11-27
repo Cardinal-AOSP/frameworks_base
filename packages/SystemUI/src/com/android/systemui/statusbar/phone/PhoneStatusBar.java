eed2615ce2d33e8f03f3499e3a280b1ce6224a30
Danesh M <daneshm90@gmail.com>
2014-11-27 14:17:19 +0100

    Battery Icon Options
    also revert
    https://github.com/AOSP-RRO/platform_frameworks_base/commit/4cf5d2c0840200e2059b1dbad40b0344418aae60
    
    Squash of following patches from cm-12.1 :
    
    Battery text mode (1/2)
    
    PS2: Make battery level more fit current solution
    PS3: Fix displaing battery level inside the icon
    PS6: Show battery level in status bar header when charging and percentage
         mode set to inside icon (it's replaced by charging icon)
    
    Change-Id: I537c99827e78c796b4980ec7751e5f9b58a88251
    Signed-off-by: kecinzer <kecinzer@gmail.com>
    
    Forward port battery icon options [1/2]
    
    Bring forward the various battery icon options.
    Portrait, Landscape, Circle, Text.
    
    Still retains new options for where to display battery percentage text.
    
    Change-Id: Icfc14d989eebde9014538291c24aa76d328df234
    
    Cleanup battery style code
    
    * Cleanup commented out code
    * Fix landscape battery icon padding
    * Fix landscape battery icon charging indicator
    * Fix landscape battery icon percentage indicator
    * Fix circle battery frame color on some black backgrounds
    * Fix circle battery artifact on low percentage
    * Fix percentage not shown while charging
    * Fix percentage always showing on expanded status bar
    * Fix battery icon showing after reboot on text/hidden modes
    
    Change-Id: Ic56878c26e9a3416149eaaa8a9325d56d664e22e
    
    SystemUI : Make BatteryMeterView multi-user compatible
    
    Change-Id: Ib9a266dc74732b7ad328a6503898a8fbc46af58e
    
    Clean up battery handling.
    
    Use a single observer for all battery meter and level views, and remove
    no-longer used callback registrations.
    
    Change-Id: I8f9db5e484628d7fbc74fe892d64bef5f08cea24
    
    SystemUI: Fix blinking when reported battery level is 0
    
    Canvas::drawArc has a bug where, if the sweepAngle is 0,
    depending on the startAngle it will draw an empty or full
    circle. In the rare cases where the battery reports 0%
    this would cause the circular meter to blink furiously
    as it animates through several values of startAngle.
    
    As a workaround, let's avoid drawing the percentage bar
    if the reported battery level is zero.
    
    Change-Id: I4c1c767f3616c9d2f8a60df63d64ec1f4e266583
    
    SystemUI: improve circle battery
    
    While plugged in, the circle animation would cause the statusbar to
    keep redrawing itself repeatedly, using up unnecessary cpu cycles,
    even when idle.
    
    - Removed circle animation in status bar
    - Left animations on keyguard/notificaiton header
    - Set layer type to hardware when animating for optimized quick drawing
    - Cleaned up some logic and unnecessary locking
    - Removed extra work done in each draw
    - override onDraw instead of draw to let the View system decide when we
      should draw
    
    Change-Id: If0624ab6e3723f4522ebbd9ce583b142c1cb4606
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
    	packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -114,6 +114,7 @@ import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.BatteryMeterView;
+import com.android.systemui.BatteryLevelTextView;
 import com.android.systemui.DemoMode;
 import com.android.systemui.EventLogConstants;
 import com.android.systemui.EventLogTags;
@@ -312,6 +313,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private boolean mWakeUpComingFromTouch;
     private PointF mWakeUpTouchLocation;
     private boolean mScreenTurningOn;
+    private BatteryMeterView mBatteryView;
+    private BatteryLevelTextView mBatteryTextView;
 
     int mPixelFormat;
     Object mQueueLock = new Object();
@@ -963,7 +966,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // Other icons
         mLocationController = new LocationControllerImpl(mContext,
                 mHandlerThread.getLooper()); // will post a notification
-        mBatteryController = new BatteryController(mContext);
+        mBatteryController = new BatteryController(mContext, mHandler);
         mBatteryController.addStateChangedCallback(new BatteryStateChangeCallback() {
             @Override
             public void onPowerSaveChanged() {
@@ -975,6 +978,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             @Override
             public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
                 // noop
+            }
+            @Override
+            public void onBatteryStyleChanged(int style, int percentMode) {
+               // noop
             }
         });
         mNetworkController = new NetworkControllerImpl(mContext, mHandlerThread.getLooper());
@@ -1075,8 +1082,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mUserInfoController.reloadUserInfo();
 
         mHeader.setBatteryController(mBatteryController);
-        ((BatteryMeterView) mStatusBarView.findViewById(R.id.battery)).setBatteryController(
-                mBatteryController);
+        BatteryMeterView batteryMeterView =
+                ((BatteryMeterView) mStatusBarView.findViewById(R.id.battery));
+        batteryMeterView.setBatteryController(mBatteryController);
+        batteryMeterView.setAnimationsEnabled(false);
+        ((BatteryLevelTextView) mStatusBarView.findViewById(R.id.battery_level_text))
+                .setBatteryController(mBatteryController);
         mKeyguardStatusBar.setBatteryController(mBatteryController);
         mHeader.setNextAlarmController(mNextAlarmController);
 
@@ -3177,6 +3188,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         if (mSecurityController != null) {
             mSecurityController.onUserSwitched(mCurrentUserId);
+        }
+        if (mBatteryController != null) {
+            mBatteryController.setUserId(mCurrentUserId);
         }
     }
 
