d433579eaac178d2ef503306b45eaf99736363d9
Danny Baumann <dannybaumann@web.de>
2014-11-30 23:04:37 -0600

    fw/base: allow home button to wake device [1/2]
    Change-Id: I2b79561dcfa7e569b2e24bbabfffb11517d4d313
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/core/java/com/android/server/policy/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3455,6 +3455,12 @@ public final class Settings {
         public static final String KEY_APP_SWITCH_LONG_PRESS_ACTION = "key_app_switch_long_press_action";
 
         /**
+         * Whether to wake the screen with the home key, the value is boolean.
+         * @hide
+         */
+        public static final String HOME_WAKE_SCREEN = "home_wake_screen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
