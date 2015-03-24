37051512e7a4b500bee2349e4a08ee311967f3b8
Meticulus <theonejohnnyd@gmail.com>
2015-03-24 15:14:17 +0100

    base: Live Volume Steps [1/2]
    add by @maxwen - add API getDefaultStreamMaxVolume
    to make clients live easier
    
    Change-Id: Ia803a6ef38e5e89e1af9a31e7904d77ec1f16722
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -794,7 +794,8 @@ public class VolumeDialogImpl implements VolumeDialog, TunerService.Tunable {
 
         // update slider max
         final int max = ss.levelMax * 100;
-        if (max != row.slider.getMax()) {
+        final boolean maxChanged = max != row.slider.getMax();
+        if (maxChanged) {
             row.slider.setMax(max);
         }
 
@@ -879,7 +880,7 @@ public class VolumeDialogImpl implements VolumeDialog, TunerService.Tunable {
         // update slider value - 0 if silent or vibrate
         final int vlevel = row.ss.muted && (isRingSilent || isNotificationSilent || isRingVibrate && !zenMuted) ? 0
                 : row.ss.level;
-        updateVolumeRowSliderH(row, enableSlider, vlevel);
+        updateVolumeRowSliderH(row, enableSlider, vlevel, maxChanged);
     }
 
     private void updateVolumeRowSliderTintH(VolumeRow row, boolean isActive) {
@@ -894,7 +895,7 @@ public class VolumeDialogImpl implements VolumeDialog, TunerService.Tunable {
         row.slider.setThumbTintList(tint);
     }
 
-    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel) {
+    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel, boolean maxChanged) {
         row.slider.setEnabled(enable);
         updateVolumeRowSliderTintH(row, row.stream == mActiveStream);
         if (row.tracking) {
@@ -918,7 +919,7 @@ public class VolumeDialogImpl implements VolumeDialog, TunerService.Tunable {
             }
         }
         final int newProgress = vlevel * 100;
-        if (progress != newProgress) {
+        if (progress != newProgress || maxChanged) {
             if (mShowing && rowVisible) {
                 // animate!
                 if (row.anim != null && row.anim.isRunning()
