0ccd6ee3cd35821ab44df97dd6541969ca3ecf3a
Adnan Begovic <adnan@cyngn.com>
2015-05-03 22:56:10 +0900

    SystemUI: HeadsUp blacklists (1/3)
    Change-Id: Ib225f87a8325cec2e63c9ef65b66b6906801ea17
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3768,6 +3768,20 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Which applications to disable heads up notifications in
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_CUSTOM_VALUES = "heads_up_custom_values";
+
+        /**
+         * Which applications to disable heads up notifications for
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- services/core/java/com/android/server/InputMethodManagerService.java
+++ services/core/java/com/android/server/InputMethodManagerService.java
@@ -2334,6 +2334,10 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
         return res;
     }
 
+    public boolean isImeShowing() {
+        return mInputShown;
+    }
+
     @Override
     public void showInputMethodPickerFromClient(
             IInputMethodClient client, int auxiliarySubtypeMode) {
