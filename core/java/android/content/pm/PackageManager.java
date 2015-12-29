7e0f7f30fa1de95ad7f5ff7f62dc4c13fe32d738
Sam Mortimer <sam@mortimer.me.uk>
2015-12-29 11:04:27 -0800

    [WIP] Add getPackageCandidateVolumesForceable() method
    Change-Id: I9cb8539615cc90cf99982c0b6706eb4cc61c0fd1

--- core/java/android/content/pm/PackageManager.java
+++ core/java/android/content/pm/PackageManager.java
@@ -4453,6 +4453,8 @@ public abstract class PackageManager {
     public abstract @Nullable VolumeInfo getPackageCurrentVolume(ApplicationInfo app);
     /** {@hide} */
     public abstract @NonNull List<VolumeInfo> getPackageCandidateVolumes(ApplicationInfo app);
+    /** {@hide} */
+    public abstract @NonNull List<VolumeInfo> getPackageCandidateVolumesForceable(ApplicationInfo app);
 
     /** {@hide} */
     public abstract int movePrimaryStorage(VolumeInfo vol);
