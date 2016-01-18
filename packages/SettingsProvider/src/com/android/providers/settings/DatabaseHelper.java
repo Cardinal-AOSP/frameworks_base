ff31c81ea32a2e75bb4320ab0bce9015e74d0cb2
fusionjack <dogfight60-fusionjack@yahoo.de>
2016-01-18 18:48:49 +0100

    Turn off mobile data while setting up the device for the first time by default
    This to avoid mobile data charges for the one who doesn't have mobile data plan.

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2632,7 +2632,7 @@ class DatabaseHelper extends SQLiteOpenHelper {
             loadSetting(stmt, Settings.Global.MOBILE_DATA,
                     "true".equalsIgnoreCase(
                             SystemProperties.get("ro.com.android.mobiledata",
-                                    "true")) ? 1 : 0);
+                                    "false")) ? 1 : 0);
 
             loadBooleanSetting(stmt, Settings.Global.NETSTATS_ENABLED,
                     R.bool.def_netstats_enabled);
