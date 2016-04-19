6c3e325bcc2b25cbb8dd192e689d053521036f75
Akhil Narang <akhilnarang.1999@gmail.com>
2016-04-19 20:04:10 +0530

    Revert "Proper supplementary service notification handling (1/5)."
    This reverts commit 5db586e15239fac5cb7bda00ba1f65a0144e9b03.

--- telecomm/java/android/telecom/ConnectionService.java
+++ telecomm/java/android/telecom/ConnectionService.java
@@ -483,16 +483,6 @@ public abstract class ConnectionService extends Service {
         }
 
         @Override
-        public void onConnectionPropertiesChanged(
-                Conference conference,
-                int connectionProperties) {
-            String id = mIdByConference.get(conference);
-            Log.d(this, "call properties: conference: %s",
-                    Connection.propertiesToString(connectionProperties));
-            mAdapter.setConnectionProperties(id, connectionProperties);
-        }
-
-        @Override
         public void onVideoStateChanged(Conference c, int videoState) {
             String id = mIdByConference.get(c);
             Log.d(this, "onVideoStateChanged set video state %d", videoState);
@@ -609,14 +599,6 @@ public abstract class ConnectionService extends Service {
         }
 
         @Override
-        public void onConnectionPropertiesChanged(Connection c, int properties) {
-            String id = mIdByConnection.get(c);
-            Log.d(this, "properties: parcelableconnection: %s",
-                    Connection.propertiesToString(properties));
-            mAdapter.setConnectionProperties(id, properties);
-        }
-
-        @Override
         public void onVideoProviderChanged(Connection c, Connection.VideoProvider videoProvider) {
             String id = mIdByConnection.get(c);
             Log.d(this, "onVideoProviderChanged: Connection: %s, VideoProvider: %s", c,
@@ -722,11 +704,10 @@ public abstract class ConnectionService extends Service {
 
         Uri address = connection.getAddress();
         String number = address == null ? "null" : address.getSchemeSpecificPart();
-        Log.v(this, "createConnection, number: %s, state: %s, capabilities: %s, properties: %s",
+        Log.v(this, "createConnection, number: %s, state: %s, capabilities: %s",
                 Connection.toLogSafePhoneNumber(number),
                 Connection.stateToString(connection.getState()),
-                Connection.capabilitiesToString(connection.getConnectionCapabilities()),
-                Connection.propertiesToString(connection.getConnectionProperties()));
+                Connection.capabilitiesToString(connection.getConnectionCapabilities()));
 
         Log.d(this, "createConnection, calling handleCreateConnectionSuccessful %s", callId);
         mAdapter.handleCreateConnectionComplete(
@@ -736,7 +717,6 @@ public abstract class ConnectionService extends Service {
                         request.getAccountHandle(),
                         connection.getState(),
                         connection.getConnectionCapabilities(),
-                        connection.getConnectionProperties(),
                         connection.getAddress(),
                         connection.getAddressPresentation(),
                         connection.getCallerDisplayName(),
@@ -1055,7 +1035,6 @@ public abstract class ConnectionService extends Service {
                     conference.getPhoneAccountHandle(),
                     conference.getState(),
                     conference.getConnectionCapabilities(),
-                    conference.getConnectionProperties(),
                     connectionIds,
                     conference.getVideoProvider() == null ?
                             null : conference.getVideoProvider().getInterface(),
@@ -1096,7 +1075,6 @@ public abstract class ConnectionService extends Service {
                     phoneAccountHandle,
                     connection.getState(),
                     connection.getConnectionCapabilities(),
-                    connection.getConnectionProperties(),
                     connection.getAddress(),
                     connection.getAddressPresentation(),
                     connection.getCallerDisplayName(),
