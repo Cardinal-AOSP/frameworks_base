ca5487a35924c6ded633fe480ae3a8b7b2a23433
srisurya95 <srisurya95@gmail.com>
2016-04-21 15:17:57 +0530

    Revert some things These are covered in advanced reboot commit
    This reverts commit e341784b26e76bef999c9bd93d4d60adcb16447b.

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -358,13 +358,8 @@ public final class ShutdownThread extends Thread {
             pd.setMessage(context.getText(com.android.internal.R.string.reboot_progress));
             pd.setIndeterminate(true);
         } else {
-            if (mReboot) {
-                pd.setTitle(context.getText(com.android.internal.R.string.reboot_title));
-                pd.setMessage(context.getText(com.android.internal.R.string.reboot_progress));
-            } else {
-                pd.setTitle(context.getText(com.android.internal.R.string.power_off));
-                pd.setMessage(context.getText(com.android.internal.R.string.shutdown_progress));
-            }
+            pd.setTitle(context.getText(com.android.internal.R.string.power_off));
+            pd.setMessage(context.getText(com.android.internal.R.string.shutdown_progress));
             pd.setIndeterminate(true);
         }
         pd.setCancelable(false);
@@ -832,24 +827,4 @@ public final class ShutdownThread extends Thread {
         }
     }
 
-    private static void deviceRebootOrShutdown(boolean reboot, String reason) {
-        Class<?> cl;
-        String deviceShutdownClassName = "com.qti.server.power.ShutdownOem";
-        try {
-            cl = Class.forName(deviceShutdownClassName);
-            Method m;
-                try {
-                    m = cl.getMethod("rebootOrShutdown", new Class[] {boolean.class, String.class});
-                    m.invoke(cl.newInstance(), reboot, reason);
-                } catch (NoSuchMethodException ex) {
-                    Log.e(TAG, "rebootOrShutdown method not found in class " + deviceShutdownClassName);
-                } catch (Exception ex) {
-                    Log.e(TAG, "Unknown exception hit while trying to invode rebootOrShutdown");
-                }
-        } catch (ClassNotFoundException e) {
-            Log.e(TAG, "Unable to find class " + deviceShutdownClassName);
-        } catch (Exception e) {
-            Log.e(TAG, "Unknown exception while trying to invoke rebootOrShutdown");
-        }
-    }
 }
