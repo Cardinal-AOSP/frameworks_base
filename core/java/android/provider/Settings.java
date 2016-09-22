0d3627cc3a24da9106a7623ec176638fde473609
Roman Birg <roman@cyngn.com>
2016-09-21 21:56:13 -0400

    SystemUI: Add double tap to sleep gesture [1/2]
    *Make it more reliable
    *Add it to keyguard
    *Add a content observer to not always query Settings.System on every touch event
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3651,6 +3651,17 @@ public final class Settings {
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
          * the setting value. See an example above.
          */
+         
+        /**
+         * Beginning of Cardinal-AOSP System Settings Additions
+         * @hide
+         */
+
+        /**
+         * Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
