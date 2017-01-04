f5a79aa17ada85d02d5857a4f0443394de1c9e84
Jon Haus <haus.xda@gmail.com>
2017-01-05 00:20:06 +0100

    Status network traffic [1/2]
    -----
    abc ezio84:
    use O darkintensity api
    use arrows vectors from aosp net arrows commit (thanks Mazda-- for this find!): https://github.com/ezio84/abc_frameworks_base/commit/ad71ae1fd09f9a7392ab2eba7b766c8b088319b4
    fix visual glitch on statusbar expand (arrows showing and quickly hiding again)
    don't show arrows when no data
    remove unit settings (KB is good)
    remove interval (set to 1sec by def) and single arrow options
    don't get settings values again when not needed
    fix padding between arrows and speed values and expose the padding value
    code cleanup
    -----
    
    * Cardinal edits:
    Disable by default
    
    Change-Id: If32bcd7764fddf00bc926b3bf6f908518b708f70

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3982,6 +3982,18 @@ public final class Settings {
         public static final String QS_TILE_TITLE_VISIBILITY = "qs_tile_title_visibility";
 
         /**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4153,6 +4165,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_COLUMNS_PORTRAIT);
             PRIVATE_SETTINGS.add(QS_COLUMNS_LANDSCAPE);
             PRIVATE_SETTINGS.add(QS_TILE_TITLE_VISIBILITY);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
         }
 
         /**
