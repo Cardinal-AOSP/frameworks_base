a82cc18c1e636343015cf22ca764d2895950aa85
xyyx <xyyx@mail.ru>
2017-02-05 14:31:26 +0800

    base: Unlock keystore with fingerprint after reboot (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3936,6 +3936,16 @@ public final class Settings {
          */
 
         /**
+         * Cardinal-AOSP System modification starts
+         */
+        
+        /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
