f696c8d7b2c1c05017898bbff7e7ff31d564d60d
cristianomatos <cristianobmatos@gmail.com>
2017-09-05 13:08:19 +0800

    Recents Clear All Button Tweaks and Add FAB to Recents Panel Thanks to RR and @varund7726
    Extracted, cleaned and updated for Cardinal by @sathish

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3938,6 +3938,18 @@ public final class Settings {
         public static final String QS_COLUMNS_LANDSCAPE = "qs_columns_landscape";
 
         /**
+         * show clear all recents button
+         * @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        /**
          * Whether to display qs tile titles in the qs panel
          * @hide
          */
