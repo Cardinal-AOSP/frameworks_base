e3db1e663f2d607974a90e8121a8cd01ef9cf17c
maxwen <max.weninger@gmail.com>
2017-09-05 20:36:26 +0200

    base: add CPU info overlay
    Change-Id: I3c75a9c2837c9293822a33a95380cc1edb7f2b1a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11158,6 +11158,12 @@ public final class Settings {
         public static final String LOCATION_SETTINGS_LINK_TO_PERMISSIONS_ENABLED =
                 "location_settings_link_to_permissions_enabled";
 
+        // omni additions start global
+        /**
+         * @hide
+         */
+        public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
         /**
          * Flag to enable use of RefactoredBackupManagerService.
          *
