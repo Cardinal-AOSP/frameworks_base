496c95bb0e898921bdbfbfce5e10b7ea750c217c
Adarsh-MR <adarshmr1998@gmail.com>
2017-09-27 07:27:06 +0000

    base: HeadsUp whitelist: force headsup for messaging and dialer apps
    credits to ezio84
    see: https://github.com/ezio84/abc_frameworks_base@f2890e3
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -6991,7 +6991,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         ComponentName componentInfo = taskInfo.get(0).topActivity;
 
         if(isPackageInWhitelist(componentInfo.getPackageName())
-                && !isDialerApp(sbn.getPackageName())) {
+                && !isImportantHeadsUp(sbn.getPackageName().toLowerCase())) {
             return false;
         }
 
@@ -7061,9 +7061,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         return mWhitelist.contains(packageName);
     }
 
-    private boolean isDialerApp(String packageName) {
-        return packageName.equals("com.android.dialer")
-            || packageName.equals("com.google.android.dialer");
+    private boolean isImportantHeadsUp(String packageName) {
+        return packageName.contains("dialer") || packageName.contains("messaging");
     }
 
     private void splitAndAddToArrayList(ArrayList<String> arrayList,
