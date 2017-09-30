95148c4554754085c30d60a6b29d0500a3948f90
BeansTown106 <nbenis106@gmail.com>
2017-09-30 13:52:26 +0000

    base: Add Vibrate on long press QSFooter buttons
    * since we dont vibrate tiles lets follow googles way and only vibrate on longpress
    
    * thanks to Alexander Martinz <eviscerationls@gmail.com> got the idea for this from his quicktiles vibrate option
      NamelessRom/android_frameworks_base@df177ae

--- packages/SystemUI/src/com/android/systemui/qs/QSFooter.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooter.java
@@ -29,6 +29,7 @@ import android.graphics.PorterDuff.Mode;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.RippleDrawable;
 import android.os.UserManager;
+import android.os.Vibrator;
 import android.provider.AlarmClock;
 import android.provider.CalendarContract.Events;
 import android.support.annotation.Nullable;
@@ -105,6 +106,7 @@ public class QSFooter extends FrameLayout implements
     private View mDateTimeGroup;
     private boolean mKeyguardShowing;
     private TouchAnimator mAlarmAnimator;
+    protected Vibrator mVibrator;
 
     public QSFooter(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -148,6 +150,8 @@ public class QSFooter extends FrameLayout implements
         mMultiUserSwitch.setOnLongClickListener(this);
         mMultiUserAvatar = mMultiUserSwitch.findViewById(R.id.multi_user_avatar);
         mAlwaysShowMultiUserSwitch = res.getBoolean(R.bool.config_alwaysShowMultiUserSwitcher);
+
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
 
         // RenderThread is doing more harm than good when touching the header (to expand quick
         // settings), so disable it for this view
@@ -197,6 +201,12 @@ public class QSFooter extends FrameLayout implements
         }
         setExpansion(mExpansionAmount);
     }
+
+    public void vibrateFooter(int duration) {
+        if (mVibrator != null) {
+            if (mVibrator.hasVibrator()) { mVibrator.vibrate(duration); }
+        }
+   }
 
     @Override
     protected void onConfigurationChanged(Configuration newConfig) {
@@ -403,6 +413,7 @@ public class QSFooter extends FrameLayout implements
         } else if (v == mEdit) {
             startEditLongClickActivity();
         }
+        vibrateFooter(20);
         return false;
     }
 
