0313b70076cb0f92f3ce9c17d8c6969c574ff238
xyyx <xyyx@mail.ru>
2017-09-30 15:08:12 +0000

    base: HeadsUpTile: longclick to launch HedasUp Settings
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/HeadsUpTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/HeadsUpTile.java
@@ -59,8 +59,9 @@ public class HeadsUpTile extends QSTileImpl<BooleanState> {
 
     @Override
     public Intent getLongClickIntent() {
-        return null;
-    }
+        return new Intent().setComponent(new ComponentName(
+            "com.android.settings", "com.android.settings.Settings$HeadsUpSettingsActivity"));
+     }
 
     @Override
     public CharSequence getTileLabel() {
