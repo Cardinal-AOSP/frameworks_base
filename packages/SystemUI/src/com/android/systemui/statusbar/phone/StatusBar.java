edb3eda8b26e465b7c6f970e72157b15ae4ff946
ezio84 <brabus84@gmail.com>
2017-11-07 17:12:16 +0100

    Navbar skiptrack arrows: blacklist some apps
    arrows are useless when playing videos with Chrome
    and a few other apps
    
    Change-Id: I375d81bfc84446db2a5543fdc5b1fdc514c1ab20

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -568,6 +568,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     protected PorterDuffXfermode mSrcOverXferMode =
             new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER);
 
+    private String[] mNavArrowsExcludeList;
     private MediaSessionManager mMediaSessionManager;
     private MediaController mMediaController;
     private String mMediaNotificationKey;
@@ -603,6 +604,12 @@ public class StatusBar extends SystemUI implements DemoMode,
                     getMediaControllerPlaybackState(mMediaController)
                     || PlaybackState.STATE_BUFFERING ==
                     getMediaControllerPlaybackState(mMediaController)) {
+                final String currentPkg = mMediaController.getPackageName().toLowerCase();
+                for (String packageName : mNavArrowsExcludeList) {
+                    if (currentPkg.contains(packageName)) {
+                        return;
+                    }
+                }
                 mNavigationBar.setTrackPlaying(true);
             } else {
                 mNavigationBar.setTrackPlaying(false);
@@ -983,6 +990,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
         };
         Dependency.get(ConfigurationController.class).addCallback(mConfigurationListener);
+
+        mNavArrowsExcludeList = mContext.getResources().getStringArray(
+                R.array.navArrowsExcludeList);
     }
 
     protected void createIconController() {
