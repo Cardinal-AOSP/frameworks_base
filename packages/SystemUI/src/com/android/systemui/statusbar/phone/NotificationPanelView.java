25827ac9eddb9ae16d0468fd2d5b6558ac8914a3
maxwen <max.weninger@gmail.com>
2018-01-21 02:06:31 +0100

    base: fix dummy expansion of qs on double tap to sleep
    qs panel gets expanded on double tap on status bar
    before screen is turned of. So stop all events once a
    double tap is detected.
    
    Change-Id: Idd75ae5a9abefecb2b98c9020a34d3065e645018

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -860,7 +860,9 @@ public class NotificationPanelView extends PanelView implements
         if (!mQsExpanded
                 && mDoubleTapToSleepEnabled
                 && event.getY() < mStatusBarHeaderHeight) {
-            mDoubleTapGesture.onTouchEvent(event);
+            if (mDoubleTapGesture.onTouchEvent(event)) {
+                return false;
+            }
         }
 
         initDownStates(event);
